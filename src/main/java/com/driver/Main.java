5e911cf1daa117adfe10e2b82aa33e87d241c2c1
MANAN SHARMA <115975287+MananSharma-git@users.noreply.github.com>
2023-05-04 01:06:08 +0530

    check4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static class A{ 
       
     public String meth(){
-                //System.out.println("A"); 
+      //System.out.println("A"); 
       return "Invoking method from class A"; 
     } 
      
@@ -13,16 +13,16 @@ public class Main {
   
   public static class B extends A{
       
-    /*@Override
+    @Override
     public String meth(){ 
-        System.out.println("B"); 
+      //System.out.println("B"); 
       return "Method is overridden in Extendend class B";
-    }*/
+    }
     
   }
 
     public static void main(String[] args) { 
-      A obj=new B(); 
+      B obj=new B(); 
       obj.meth();
       //System.out.print(obj.meth()); 
     } 
